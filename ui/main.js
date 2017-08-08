96f27d2af2993b55dd2d6eb73e6f25fbe4162a69
georgeantoney333 <30750962+georgeantoney333@users.noreply.github.com>
2017-08-08 17:22:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 //move element
-var img = document.getElementById("madi");
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft = "100px";
-}
+};
