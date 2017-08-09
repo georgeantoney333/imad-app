0bd88138c4338a729e1705cdedd26ee758ffe584
georgeantoney333 <30750962+georgeantoney333@users.noreply.github.com>
2017-08-09 08:49:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,6 +31,8 @@ button.onclick = function()
         if(request.status === 200)
         {
             var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
         }
         
     }
