25cb3794e048e03fc039471bd93e1183789b54db
georgeantoney333 <30750962+georgeantoney333@users.noreply.github.com>
2017-08-17 10:05:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,8 +49,26 @@ app.get('/submit-name', function (req, res) { // /submit-name/name-xxx
    
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/article/:articleName', function (req, res)
+{
+  //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result)
+    {
+        if (err)
+        {
+            res.status(500).send(err.toString());
+            
+        }
+        else
+            if (result.rows.length === 0)
+            {
+                result.status(404).send('Article Not Found');
+            } else
+            {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+    });
 });
 
 
