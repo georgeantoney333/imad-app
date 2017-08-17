6c058b4852671d6e25246809a19de1aa3065c230
georgeantoney333 <30750962+georgeantoney333@users.noreply.github.com>
2017-08-17 09:10:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db', function (req, res) {
+  //make a select request
+  
+  //return response
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
